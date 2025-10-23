2566fd2bbaa2a3ab18f2116f8af03fc327245498
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-10-24 01:13:30 +0300

    feat: make startsAt and endsAt field responsive on mobile screens

--- client/src/components/ui/quiz/PostQuiz.tsx
+++ client/src/components/ui/quiz/PostQuiz.tsx
@@ -196,7 +196,10 @@ export const PostQuiz: React.FC = () => {
           formik={formik}
           required={true}
         />
-        <div className="w-full flex items-center justify-center gap-4">
+        <div
+          className="w-full flex flex-col sm:flex-row items-center justify-center 
+          gap-4"
+        >
           {/* Starts At */}
           <div className="w-full">
             <h3 className="text-gray-500 text-[12px]">Starts At</h3>
