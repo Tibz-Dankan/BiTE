018bd69515b9dd27c000e3d903438c212feae730
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-12-12 18:00:10 +0300

    feat: validate quiz for questions and answers on making it attemptable

--- server/internal/handlers/quiz/makeQuizAttemptable.go
+++ server/internal/handlers/quiz/makeQuizAttemptable.go
@@ -1,16 +1,19 @@
 package quiz
 
 import (
+	"fmt"
+	"log"
+	"time"
+
 	"github.com/Tibz-Dankan/BiTE/internal/models"
 	"github.com/gofiber/fiber/v2"
 )
 
 var MakeQuizAttemptable = func(c *fiber.Ctx) error {
 	quiz := models.Quiz{}
-	attachment := models.Attachment{}
 	quizID := c.Params("id")
 
-	quiz, err := quiz.FindOne(quizID)
+	quiz, err := quiz.FindOneWithQuestionsAndAnswers(quizID)
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
@@ -22,22 +25,42 @@ var MakeQuizAttemptable = func(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, "Quiz is already attemptable!")
 	}
 
-	// TODO: To ensure the quiz, has at least a question
-	// and each question has an at least two answers
+	if len(quiz.Questions) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Quiz must have at least one question!")
+	}
+
+	for _, question := range quiz.Questions {
+		if len(question.Answers) == 0 {
+			errMessage := fmt.Sprintf("Question '%s' must have at least one answer!", question.Title)
+			return fiber.NewError(fiber.StatusBadRequest, errMessage)
+		}
+
+		var correctAnswerFound bool
+		for _, answer := range question.Answers {
+			log.Printf("answer: %+v", answer)
+			if answer.IsCorrect {
+				correctAnswerFound = true
+				break
+			}
+		}
+		if !correctAnswerFound {
+			errMessage := fmt.Sprintf("Question '%s' must have at least one correct answer!", question.Title)
+			return fiber.NewError(fiber.StatusBadRequest, errMessage)
+		}
+	}
+
+	now := time.Now()
+	if quiz.EndsAt.Before(now) {
+		return fiber.NewError(fiber.StatusBadRequest,
+			"Quiz can't be attemptable if it has already ended!")
+	}
 
 	updatedQuiz, err := quiz.UpdateCanBeAttempted(quizID, true)
 	if err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
-	savedAttachments, err := attachment.FindAllByQuiz(quizID, 20, "")
-	if err != nil {
-		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
-	}
-
-	for _, savedAttachment := range savedAttachments {
-		updatedQuiz.Attachments = append(updatedQuiz.Attachments, &savedAttachment)
-	}
+	updatedQuiz.Attachments = quiz.Attachments
 
 	response := fiber.Map{
 		"status":  "success",
