3c98a9a1690aa86b2d6637d9b06203b4c68b3166
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-12-20 20:49:18 +0300

    feat: check jwt expired and should refresh checks

--- client/src/hooks/useSigninWithRefreshToken.tsx
+++ client/src/hooks/useSigninWithRefreshToken.tsx
@@ -2,11 +2,39 @@ import { useMutation } from "@tanstack/react-query";
 import { useAuthStore } from "../stores/auth";
 import { authAPI } from "../api/auth";
 import type { TAuth } from "../types/auth";
-// import { useCallback } from "react";
+import { useCallback } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export const useSigninWithRefreshToken = () => {
   const auth = useAuthStore((state) => state.auth);
   const updateAuth = useAuthStore((state) => state.updateAuth);
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+
+  const isTokenExpired = useCallback((token: string): boolean => {
+    if (!token) return true;
+
+    const payload = jwtDecode(token);
+    if (!payload || !payload.exp) return true;
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    return payload.exp <= currentTime;
+  }, []);
+
+  const shouldRefreshToken = useCallback((token: string): boolean => {
+    if (!token) return false;
+
+    const payload = jwtDecode(token);
+    const refreshThreshold = 15 * 60 * 1000;
+
+    if (!payload || !payload.exp) return false;
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    const expirationTime = payload.exp;
+    const timeUntilExpiration = (expirationTime - currentTime) * 1000; // Convert to milliseconds
+
+    // Token is expired or will expire within the threshold
+    return timeUntilExpiration <= refreshThreshold;
+  }, []);
 
   const { mutate } = useMutation({
     mutationFn: authAPI.signinWithRefreshToken,
@@ -16,39 +44,22 @@ export const useSigninWithRefreshToken = () => {
     },
     onError: (error: any) => {
       console.error("Failed to signin with refresh token:", error.message);
+      if (isTokenExpired(auth.refreshToken)) {
+        clearAuth();
+      }
     },
   });
 
-  //     const isTokenExpired = useCallback((token: string): boolean => {
-  //     if (!token) return true;
-
-  //     const payload = jwtDecode(token);
-  //     if (!payload || !payload.exp) return true;
-
-  //     const currentTime = Math.floor(Date.now() / 1000);
-  //     return payload.exp <= currentTime;
-  //   }, []);
-
-  //     const shouldRefreshToken = useCallback(
-  //     (token: string): boolean => {
-  //       if (!token) return false;
-
-  //       const payload = jwtDecode(token);
-
-  //       if (!payload || !payload.exp) return false;
-
-  //       const currentTime = Math.floor(Date.now() / 1000);
-  //       const expirationTime = payload.exp;
-  //       const timeUntilExpiration = (expirationTime - currentTime) * 1000; // Convert to milliseconds
-
-  //       // Token is expired or will expire within the threshold
-  //       return timeUntilExpiration <= refreshThreshold;
-  //     },
-  //     [refreshThreshold]
-  //   );
-
   const logInWithRT = async () => {
-    mutate({ userID: auth.user.id, refreshToken: auth.refreshToken });
+    if (isTokenExpired(auth.accessToken)) {
+      console.log("Token expired");
+      mutate({ userID: auth.user.id, refreshToken: auth.refreshToken });
+      return;
+    }
+    if (shouldRefreshToken(auth.accessToken)) {
+      console.log("Token ready for refresh");
+      mutate({ userID: auth.user.id, refreshToken: auth.refreshToken });
+    }
   };
 
   return { logInWithRT };
