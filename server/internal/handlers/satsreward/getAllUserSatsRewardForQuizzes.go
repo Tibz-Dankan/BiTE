a364f3442ee694dfe4ea9d1a523a5a870cf0c813
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-02-18 02:23:31 +0300

    feat: add endpoint GetAllUserSatsRewardForQuizzes

---
+++ server/internal/handlers/satsreward/getAllUserSatsRewardForQuizzes.go
@@ -0,0 +1,42 @@
+package satsreward
+
+import (
+	"github.com/Tibz-Dankan/BiTE/internal/models"
+	"github.com/Tibz-Dankan/BiTE/internal/pkg"
+	"github.com/gofiber/fiber/v2"
+)
+
+var GetAllUserSatsRewardForQuizzes = func(c *fiber.Ctx) error {
+	satsReward := models.SatsReward{}
+	limitParam := c.Query("limit")
+	cursorParam := c.Query("cursor")
+	userID := c.Params("userID")
+
+	limit, err := pkg.ValidateQueryLimit(limitParam)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+	if cursorParam == "" {
+		cursorParam = ""
+	}
+
+	if userID != c.Locals("userID").(string) {
+		return fiber.NewError(fiber.StatusForbidden, "Invalid userID provided!")
+	}
+
+	userQuizProgresses, pagination, err := satsReward.FindAllSatsClaimForUser(
+		limit,
+		cursorParam,
+		userID,
+	)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"status":     "success",
+		"message":    "Quizzes to be rewarded with sats fetched successfully!",
+		"data":       userQuizProgresses,
+		"pagination": pagination,
+	})
+}
