02fe10f95cb64a15f9653802bb3ddc40195714f9
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-02-22 15:29:08 +0300

    feat: display user's claimable sats in the dashboard header

--- client/src/components/ui/quiz/UserQuizProgressInfo.tsx
+++ client/src/components/ui/quiz/UserQuizProgressInfo.tsx
@@ -2,23 +2,53 @@ import { useEffect, useState } from "react";
 import { Button } from "../shared/Btn";
 import { useQuery } from "@tanstack/react-query";
 import { AlertCard } from "../shared/AlertCard";
-import { Loader2 } from "lucide-react";
+import { Gift, Loader2 } from "lucide-react";
 import { quizAPI } from "../../../api/quiz";
 import { useAuthStore } from "../../../stores/auth";
 import type { TQuizUserProgressCount } from "../../../types/quiz";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFeatureFlagEnabled } from "@posthog/react";
+import { satsRewardAPI } from "../../../api/satsReward";
 
 export const UserQuizProgressInfo: React.FC = () => {
   const [quizProgressStatus, setQuizProgressStatus] = useState("IN_PROGRESS");
   const user = useAuthStore((state) => state.auth.user);
   const navigate = useNavigate();
+  const isSatsRewardEnabled = useFeatureFlagEnabled("sats-reward");
 
-  const { data, isPending, isError, error } = useQuery({
+  const {
+    data: quizUserProgressData,
+    isPending: isPendingQuizUserProgress,
+    isError: isErrorQuizUserProgress,
+    error: errorQuizUserProgress,
+  } = useQuery({
     queryKey: ["quiz-user-progress", user.id, quizProgressStatus],
     queryFn: () => quizAPI.getQuizUserProgressCount({ userID: user.id }),
   });
 
-  const quizUserProgressCount: TQuizUserProgressCount = data?.data ?? {};
+  const quizUserProgressCount: TQuizUserProgressCount =
+    quizUserProgressData?.data ?? {};
+
+  const {
+    isPending: isPendingUserSatsRewardStats,
+    isError: isErrorUserSatsRewardStats,
+    data: userSatsRewardStatsData,
+    error: errorUserSatsRewardStats,
+  } = useQuery({
+    queryKey: ["userSatsRewardStats", user.id],
+    queryFn: () => satsRewardAPI.getUserStats({ userID: user.id }),
+  });
+
+  const statsData = userSatsRewardStatsData?.data ?? {
+    totalSatsToBeClaimed: 0,
+    totalSatsEarned: 0,
+  };
+  const hasUnclaimedSats = statsData.totalSatsToBeClaimed > 0;
+  const showUnclaimedSatsInfo = hasUnclaimedSats && isSatsRewardEnabled;
+
+  const isPending = isPendingQuizUserProgress || isPendingUserSatsRewardStats;
+  const isError = isErrorQuizUserProgress || isErrorUserSatsRewardStats;
+  const error = errorQuizUserProgress || errorUserSatsRewardStats;
 
   const showInProgressInfo =
     quizUserProgressCount.inProgressCount > 0 &&
@@ -38,10 +68,10 @@ export const UserQuizProgressInfo: React.FC = () => {
   };
 
   useEffect(() => {
-    if (data?.data?.inProgressCount === 0) {
+    if (quizUserProgressData?.data?.inProgressCount === 0) {
       setQuizProgressStatus(() => "COMPLETED");
     }
-  }, [data]);
+  }, [quizUserProgressData]);
 
   if (isPending) {
     return (
@@ -63,74 +93,117 @@ export const UserQuizProgressInfo: React.FC = () => {
   }
 
   return (
-    <div
-      className="w-full flex flex-col sm:flex-row gap-4 border-1s
-            border-gray-300 rounded-lg p-4 text-gray-600 bg-gray-800/8"
-    >
-      {/* InProgressInfo */}
-      {showInProgressInfo && (
-        <div className="w-full flex items-center gap-2">
-          <span className="text-sm">
-            Hi {user.name}, ⏳ you have {quizUserProgressCount.inProgressCount}{" "}
-            unfinished{" "}
-            {quizUserProgressCount.inProgressCount === 1 ? "quiz" : "quizzes"}.
-            Please continue attempting to complete{" "}
-            {quizUserProgressCount.inProgressCount === 1 ? "it" : "them"}
-          </span>
+    <div className="w-full">
+      {showUnclaimedSatsInfo && (
+        <div
+          className="w-full flex flex-col sm:flex-row gap-4 border-1s
+         border-gray-300 rounded-lg p-4 text-gray-600 bg-gray-800/8"
+        >
+          <div className="w-full flex items-center gap-2">
+            <p className="w-full flex items-center gap-2 text-sm">
+              <span>
+                <Gift className="w-4 h-4 text-(--primary)" />
+              </span>
+              <span>
+                Hi {user.name}, 💰 you have{" "}
+                <span className="font-semibold text-gray-800">
+                  {statsData.totalSatsToBeClaimed} Satoshis
+                </span>{" "}
+                to be claimed as you reward.{" "}
+                <Link
+                  to="/rewards"
+                  className="text-(--primary) hover:underline"
+                >
+                  Learn more
+                </Link>
+              </span>
+            </p>
+          </div>
+          <Button
+            type="button"
+            onClick={() => navigateTo(`/u/rewards`)}
+            className={`rounded-lg px-4 py-2 bg-(--primary)
+           text-gray-50 hover:cursor-pointer self-end`}
+          >
+            <span>Claim</span>
+          </Button>
         </div>
       )}
-      {showInProgressInfo && (
-        <Button
-          type="button"
-          onClick={() => navigateTo(`/u/quizzes?qzpStatus=in_progress`)}
-          className={`rounded-lg px-4 py-2 bg-(--primary)
-                         text-gray-50 hover:cursor-pointer self-end`}
+      {!showUnclaimedSatsInfo && (
+        <div
+          className="w-full flex flex-col sm:flex-row gap-4 border-1s
+         border-gray-300 rounded-lg p-4 text-gray-600 bg-gray-800/8"
         >
-          <span>Continue</span>
-        </Button>
-      )}
+          {/* InProgressInfo */}
+          {showInProgressInfo && (
+            <div className="w-full flex items-center gap-2">
+              <span className="text-sm">
+                Hi {user.name}, ⏳ you have{" "}
+                {quizUserProgressCount.inProgressCount} unfinished{" "}
+                {quizUserProgressCount.inProgressCount === 1
+                  ? "quiz"
+                  : "quizzes"}
+                . Please continue attempting to complete{" "}
+                {quizUserProgressCount.inProgressCount === 1 ? "it" : "them"}
+              </span>
+            </div>
+          )}
+          {showInProgressInfo && (
+            <Button
+              type="button"
+              onClick={() => navigateTo(`/u/quizzes?qzpStatus=in_progress`)}
+              className={`rounded-lg px-4 py-2 bg-(--primary)
+                         text-gray-50 hover:cursor-pointer self-end`}
+            >
+              <span>Continue</span>
+            </Button>
+          )}
 
-      {/* CompletedInfo */}
-      {showCompletedInfo && (
-        <div className="w-full flex items-center gap-2">
-          <span className="text-sm">
-            Hi {user.name}, 🏆 you have {quizUserProgressCount.completedCount}{" "}
-            completed{" "}
-            {quizUserProgressCount.completedCount === 1 ? "quiz" : "quizzes"}.
-            Great job! Attempt more quizzes to improve your ranking
-          </span>
+          {/* CompletedInfo */}
+          {showCompletedInfo && (
+            <div className="w-full flex items-center gap-2">
+              <span className="text-sm">
+                Hi {user.name}, 🏆 you have{" "}
+                {quizUserProgressCount.completedCount} completed{" "}
+                {quizUserProgressCount.completedCount === 1
+                  ? "quiz"
+                  : "quizzes"}
+                . Great job! Attempt more quizzes to improve your ranking
+              </span>
+            </div>
+          )}
+          {showCompletedInfo && (
+            <Button
+              type="button"
+              onClick={() => navigateTo(`/u/quizzes?qzpStatus=un_attempted`)}
+              className={`rounded-lg px-4 py-2 bg-(--primary)
+                         text-gray-50 hover:cursor-pointer self-end`}
+            >
+              <span>Attempt</span>
+            </Button>
+          )}
+
+          {/* UnattemptedInfo */}
+          {showUnattemptedInfo && (
+            <div className="w-full flex items-center gap-2">
+              <span className="text-sm">
+                Hi {user.name}, 📝 you have not attempted any quizzes yet. Start
+                attempting quizzes to improve your ranking.
+              </span>
+            </div>
+          )}
+
+          {showUnattemptedInfo && (
+            <Button
+              type="button"
+              onClick={() => navigateTo(`/u/quizzes?qzpStatus=un_attempted`)}
+              className={`rounded-lg px-4 py-2 bg-(--primary)
+              text-gray-50 hover:cursor-pointer self-end`}
+            >
+              <span>Start</span>
+            </Button>
+          )}
         </div>
-      )}
-      {showCompletedInfo && (
-        <Button
-          type="button"
-          onClick={() => navigateTo(`/u/quizzes?qzpStatus=un_attempted`)}
-          className={`rounded-lg px-4 py-2 bg-(--primary)
-                         text-gray-50 hover:cursor-pointer self-end`}
-        >
-          <span>Attempt</span>
-        </Button>
-      )}
-
-      {/* UnattemptedInfo */}
-      {showUnattemptedInfo && (
-        <div className="w-full flex items-center gap-2">
-          <span className="text-sm">
-            Hi {user.name}, 📝 you have not attempted any quizzes yet. Start
-            attempting quizzes to improve your ranking.
-          </span>
-        </div>
-      )}
-
-      {showUnattemptedInfo && (
-        <Button
-          type="button"
-          onClick={() => navigateTo(`/u/quizzes?qzpStatus=un_attempted`)}
-          className={`rounded-lg px-4 py-2 bg-(--primary)
-                         text-gray-50 hover:cursor-pointer self-end`}
-        >
-          <span>Start</span>
-        </Button>
       )}
     </div>
   );
