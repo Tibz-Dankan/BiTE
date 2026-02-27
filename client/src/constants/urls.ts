2b8e4bf743eea6b578d22cdede94a1b1d529b3d0
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-02-27 22:51:17 +0300

    refactor: switch back the bitcoinhighschool associated backend api url

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -3,8 +3,8 @@ let SERVER_URL: string;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   SERVER_URL = "http://localhost:5000/api/v1";
 } else {
-  // SERVER_URL = "https://api.bitcoinhighschool.com/api/v1";
-  SERVER_URL = "https://bite-a70i.onrender.com/api/v1";
+  SERVER_URL = "https://api.bitcoinhighschool.com/api/v1";
+  // SERVER_URL = "https://bite-a70i.onrender.com/api/v1";
 }
 
 export { SERVER_URL };
