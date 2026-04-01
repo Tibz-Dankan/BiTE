48d049141c770c61b84d133d8de3ce99f76440bf
Tibz-Dankan <tibesigwadankan@gmail.com>
2026-04-01 20:52:48 +0300

    refactor: improve answer result table headings

--- client/src/components/ui/quiz/UserAnswerResultTable.tsx
+++ client/src/components/ui/quiz/UserAnswerResultTable.tsx
@@ -48,7 +48,7 @@ export const UserAnswerResultTable: React.FC<UserAnswerResultTableProps> = ({
               Your Answer(s)
             </th>
             <th className="py-2 px-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-200">
-              Answers
+              Answer(s)
             </th>
           </tr>
         </thead>
